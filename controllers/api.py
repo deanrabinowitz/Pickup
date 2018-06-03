f13f4411fe53aec4786963ec606094a38863dc78
Nir Avrahamov <niravrahamov@gmail.com>
2018-06-03 15:12:59 -0700

    added game_level to get_games in api

--- controllers/api.py
+++ controllers/api.py
@@ -14,6 +14,7 @@ def get_games():
                 userEmail = r.user_email,
                 title = r.title,
                 activity = r.activity,
+                gameLevel = r.game_level,
                 gameTime = r.game_time,
                 gameLocation = r.game_location,
                 description = r.description,
